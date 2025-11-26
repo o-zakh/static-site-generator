d440314de21ee708faf8bcce61104772124f4bae
oleg <o.zakharov@nexters.com>
2025-11-26 04:13:01 +0400

    CH2L2: Integrated textnodes

---
+++ src/textnode.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+class TextType(Enum):
+    TEXT = "text"
+    BOLD = "**Bold text**"
+    ITALIC = "_Italic text_"
+    CODE = "`Code text`"
+    LINK = "[anchor text](url)"
+    IMAGE = "![alt text](url)"
+
+class TextNode():
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other_text_node):
+        return (
+            self.text == other_text_node.text and 
+            self.text_type == other_text_node.text_type and
+            self.url == other_text_node.url
+        ) 
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
